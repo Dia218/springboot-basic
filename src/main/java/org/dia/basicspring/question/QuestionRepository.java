867408b4c63f56402543fc861e52597b49950b31
dia218 <sang218@kyonggi.ac.kr>
2025-01-02 01:03:07 +0900

    feat(navbar): Implement navbar
     - next is to implement login.

--- src/main/java/org/dia/basicspring/question/QuestionRepository.java
+++ src/main/java/org/dia/basicspring/question/QuestionRepository.java
@@ -1,11 +1,17 @@
 package org.dia.basicspring.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
+    
     Question findBySubjectAndContent(String subject, String Content);
+    
     List<Question> findBySubjectLike(String subject);
+    
+    Page<Question> findAll(Pageable pageable);
 }
